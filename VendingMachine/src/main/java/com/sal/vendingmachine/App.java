570bee948938fed3a4078e3175936cbc24f83128
Everlyn <eveeleon@gmail.com>
2022-11-02 22:42:57 -0700

    Maven troubleshooting

--- VendingMachine/src/main/java/com/sal/vendingmachine/App.java
+++ VendingMachine/src/main/java/com/sal/vendingmachine/App.java
@@ -3,16 +3,9 @@ package com.sal.vendingmachine;
 
 //Adding springframework
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.sal.vendingmachine.controller.VendingMachineController;
 import com.sal.vendingmachine.dao.VendingMachineException;
-import com.sal.vendingmachine.service.VendingMachineService;
-import com.sal.vendingmachine.service.VendingMachineServiceImpl;
-import com.sal.vendingmachine.ui.UserIO;
-import com.sal.vendingmachine.ui.UserIOImpl;
-import com.sal.vendingmachine.ui.VendingMachineView;
 import com.sal.vendingmachine.controller.VendingMachineController;
 
 
